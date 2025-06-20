7b8868d78ebcb6a584e10e49bfce333a2551dfd8
Mohannad Jaradat <jaradatm2@hotmail.com>
2025-06-20 14:15:10 +0300

    test

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("Hello from Jenkins CI/CD")
 st.write("This is a live-deployed Streamlit app.")
-st.write("Is this going to be deployed???")
+st.write("I need to trigger the pipeline on push")
