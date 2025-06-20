050b95e5c91ad604afabf0931e454d1b2c659e50
Mohannad Jaradat <jaradatm2@hotmail.com>
2025-06-20 13:29:33 +0300

    edit deply.sh

--- streamlit_app/deploy.sh
+++ streamlit_app/deploy.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-APP_DIR="/srv/streamlit_app"
-REPO_URL="git@github.com:mohannad-jaradat/simple_jenkins.git"
+APP_DIR="/srv/streamlit_app/simple_jenkins/streamlit_app"
+REPO_URL="git@github.com:MohannadmJaradat/simple_jenkins.git"
 BRANCH=$1
 
 echo "Deploying branch: $BRANCH"
 
 # If folder doesn't exist, clone it
 if [ ! -d "$APP_DIR" ]; then
-    git clone -b "$BRANCH" "$REPO_URL" "$APP_DIR"
+    git clone -b main git@github.com:MohannadmJaradat/simple_jenkins.git /srv/streamlit_app/simple_jenkins
 else
-    cd "$APP_DIR"
-    git pull origin "$BRANCH"
+    cd /srv/streamlit_app/simple_jenkins
+    git pull origin main
 fi
 
 # Install deps and run Streamlit
 cd "$APP_DIR"
+. venv/bin/activate
 pip3 install -r requirements.txt
 pkill streamlit || true
 nohup streamlit run app.py --server.port 8501 > streamlit.log 2>&1 &
